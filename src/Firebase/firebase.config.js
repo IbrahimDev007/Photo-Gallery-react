2c88d4c98988397e4055761f17bfceaa4c8b667d
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-11-04 19:00:48 +0600

    firenase add

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBbOBNO7Xmml44oY-QvDc_m0oDYBAmDDx4",
+    authDomain: "image-gallery-bc2a6.firebaseapp.com",
+    projectId: "image-gallery-bc2a6",
+    storageBucket: "image-gallery-bc2a6.appspot.com",
+    messagingSenderId: "259346912350",
+    appId: "1:259346912350:web:1b38d2bd45d805b48810cf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
