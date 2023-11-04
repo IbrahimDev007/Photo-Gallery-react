bdf1bea28979dcdd469007ca099790295de6ebf0
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-11-04 20:17:55 +0600

    some change

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,7 +2,7 @@ import Swal from "sweetalert2";
 import useAuthHook from "../Hook/useAuthHook";
 import { FcGoogle } from "react-icons/fc";
 const Login = () => {
-	const { user, googleSignIn, logOut } = useAuthHook();
+	const { user, googleSignIn, logOut, loading } = useAuthHook();
 
 	const handleGoogle = () => {
 		if (user) {
@@ -36,26 +36,31 @@ const Login = () => {
 
 	return (
 		<div>
-			{user ? (
-				<div>
-					<img
-						src={user.photoURL || "default-avatar-image.jpg"} // Use a default image if user's photoURL is not available
-						alt="User Avatar"
-						className="rounded-full w-16 h-16"
-					/>
-					<button className="btn " onClick={handleGoogle}>
+			{loading ? (
+				<p>Loading...</p>
+			) : user ? (
+				<div className="flex justify-center items-center flex-col">
+					<div className="avatar online">
+						<div className="w-12 rounded-full">
+							<img src={user.photoURL} alt="User Avatar" />
+						</div>
+					</div>
+					<button
+						className="btn btn-sm btn-error btn-outline"
+						onClick={handleGoogle}
+					>
 						Logout
 					</button>
 				</div>
 			) : (
-				<>
+				<div>
 					<button
 						onClick={handleGoogle}
 						className="btn btn-square btn-success btn-outline  text-xs flex items-center justify-center"
 					>
 						<FcGoogle className="text-4xl" />
 					</button>
-				</>
+				</div>
 			)}
 		</div>
 	);
