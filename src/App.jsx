abf642475391c61006065498adc084ce70771ee0
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-11-04 20:22:53 +0600

    header change

--- src/App.jsx
+++ src/App.jsx
@@ -52,20 +52,26 @@ const App = () => {
 		<div className="mx-auto max-w-screen-xl">
 			<header className="flex justify-between items-center">
 				{count > 0 ? (
-					<div className="flex items-center">
-						<input type="checkbox" checked="checked" />
-						<span className="font-medium text-lg">{count} Files Selected</span>
-					</div>
+					<>
+						<div className="flex items-center">
+							<input type="checkbox" checked={true} />
+							<span className="font-medium text-lg">
+								{count} Files Selected
+							</span>
+						</div>
+						<button
+							className="hover:underline text-red-500 text-xl font-medium "
+							onClick={deleteSelectedItems}
+						>
+							Delete
+						</button>
+					</>
 				) : (
-					<h1 className="font-medium text-lg">Gallery</h1>
+					<>
+						<h1 className="font-medium text-lg">Gallery</h1>
+						<Login />
+					</>
 				)}
-				<button
-					className="hover:underline text-red-500 font-normal "
-					onClick={deleteSelectedItems}
-				>
-					Delete
-				</button>
-				<Login />
 			</header>
 			<div className="divider"></div>
 
